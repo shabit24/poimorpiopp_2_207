e00765873bdcf74eaf5b338f1343b3527e1f4c86
shabit24 <hossain.shabit24@gmail.com>
2023-05-29 18:53:40 +0700

    Add project files.

---
+++ multiwaris1.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class orang {
+public:
+	string nama;
+
+	orang(string pNama) :
+		nama(pNama) {
+		cout << "orang dibuat\n" << endl;
+
+	}
+	~orang() {
+		cout << "orang dihapus\n" << endl;
+	}
+};
+
+class manusia {
+public:
+	string jenisKelamin;
+	manusia(string pJenisKelamin) :
+		jenisKelamin(pJenisKelamin) {
+		cout << "manusia dibuat\n" << endl;
+
+	}
+	~manusia() {
+		cout << "manusia dihapus" << endl;
+	}
+
+};
+
+class pelajar : private manusia, public orang {
+public:
+	string sekolah;
+	pelajar(string pNama, string pJenisKelamin, string pSekolah) :
+		orang(pNama),
+		manusia(pJenisKelamin),
+		sekolah(pSekolah) {
+		cout << "pelajer dibuat\n" << endl;
+
+	}
+	~pelajar() {
+		cout << "pelajar dihapus" << endl;
+	}
+	string perkenalan() {
+		return "Hallo, nama saya " + nama + "dengan jenis kelamin" + jenisKelamin + "dari sekolah " + sekolah + "\n\n";
+	}
+};
+
+int main() {
+	pelajar andi("andi laksono", "Laki-Laki", "belajarcpp");
+	cout << andi.perkenalan();
+	return 0;
+}
+
